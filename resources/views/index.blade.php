9779786f9b9771c5fa42b89fc8c7150bd40c3b8c
JonatasSantos <jonatasrafael97@gmail.com>
2021-11-29 16:23:45 -0300

    Limpando codigo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -32,7 +32,6 @@
             </nav>
         </div>
     </header>
-    {{-- @if(isset($array)) { --}}
         <div id="mainContent" class="container">
             <div class="container col-12">
                 <h2>Previsao para {{ $array[0]->name }}</h2>
@@ -66,67 +65,29 @@
                 
             </div>
         </div>
-        {{-- @else
-            <div id="mainContent" class="container">
-                <div class="container col-12">
-                    <h2>Previsao para {{ $array[0]->name }}</h2>
-                    @for($i = 1; $i < count($array); $i++) 
-                        <div id="card" class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">{{ $array[$i]->date }}</h5>
-                                <p class="card-text">{{ $array[$i]->text }}</p>
-                            
-                            </div>
-                            <div class="card-body cardBody2" style="background-color: #dee2e6; width: 100%;">
-                                <div class="row card-row1">
-                                    <div class="col-6">
-                                    <span><img src="/images/icons/upload.png" alt="maxima">&nbsp;{{ $array[$i]->temperature_max }}&#176;C</span>
-                                    </div>
-                                    <div class="col-6">
-                                        <span><img src="/images/icons/download.png" alt="minima">&nbsp;{{$array[$i]->temperature_min}}&#176;C</span>
-                                    </div>
-                                </div>
-                                <div class="row card-row2">
-                                    <div class="col-6">
-                                        <span><img src="/images/icons/raindrop-close-up.png" alt="umidade">&nbsp;{{$array[$i]->rain_precipitation}}&#176;C</span>
-                                    </div>
-                                    <div class="col-6">
-                                        <span><img src="/images/icons/umbrella.png" alt="chuva">&nbsp;{{$array[$i]->rain_probability}}&#176;C</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    @endfor
-                    
-                </div>
-            </div>
-    @endif --}}
-    
-        
-       
     <script>
         let input = document.getElementById("idInput");
         console.log(input);
         input.addEventListener("keydown", (e) => {
         
-        if (e.keyCode === 13) {
-            let url = 'http://localhost:8001/weather/' + input.value;
-            let dados = <?php echo json_encode($array); ?>;
-            
-            location.href = url;
+            if (e.keyCode === 13) {
+                let url = 'http://localhost:8001/weather/' + input.value;
+                let dados = <?php echo json_encode($array); ?>;
+                
+                location.href = url;
 
-            if (dados != undefined) {
-                e.preventDefault();
-            // alert(input.value);
-                document.getElementById("mainContent").style.display="block";    
-                console.log(dados); 
-            } else {
-                alert("Algum erro ocorreu.");
+                if (dados != undefined) {
+                    e.preventDefault();
+                // alert(input.value);
+                    document.getElementById("mainContent").style.display="block";    
+                    console.log(dados); 
+                } else {
+                    alert("Algum erro ocorreu.");
+                }
+                
             }
-            
-        }
 
-});
+        });
 
     </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
